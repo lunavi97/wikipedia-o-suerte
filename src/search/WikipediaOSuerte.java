ef4b720496a5ab854889e270c0ee79daa370bae6
Luciano Nahuel Vitale <lunavi97@gmail.com>
2018-06-09 04:30:29 -0300

    Devolver como respuesta la URL de Wikipedia si existe el artículo o la URL de búsqueda de voy a tener suerte

--- src/search/WikipediaOSuerte.java
+++ src/search/WikipediaOSuerte.java
@@ -1,5 +1,51 @@
 package search;
 
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.net.URLConnection;
+
 public class WikipediaOSuerte {
 
+	public String solve(String tema) throws IOException {
+		String respuesta;
+		
+		try {
+			respuesta = buscarEnWikipedia(tema);
+		} catch(FileNotFoundException e) {
+			respuesta = voyATenerSuerte(tema);
+		}
+		
+		return respuesta;
+	}
+	
+	private String buscarEnWikipedia(String tema) throws IOException {
+		String urlString = "https://es.wikipedia.org/wiki/" + tema;
+		getContent(urlString);
+		return urlString;
+	}
+	
+	private String voyATenerSuerte(String tema) throws MalformedURLException {
+		return "https://google.com.ar/search?btnI&q=" + tema;
+	}
+	
+	private String getContent(String urlString) throws IOException {
+		System.setProperty("http.agent", "Brave");
+		URL url = new URL(urlString);
+		URLConnection urlConnection = url.openConnection();
+		InputStream is = urlConnection.getInputStream();
+		BufferedReader br = new BufferedReader(new InputStreamReader(is));
+		String content = "";
+		String linea = br.readLine();
+		while(linea != null) {
+			content += linea;
+			linea = br.readLine();
+		}
+		return content;
+	}
+	
 }
--- test/search/WikipediaOSuerteTests.java
+++ test/search/WikipediaOSuerteTests.java
@@ -1,14 +1,31 @@
 package search;
 
-import static org.junit.Assert.*;
+import java.io.IOException;
 
+import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 
 public class WikipediaOSuerteTests {
 
+	WikipediaOSuerte wos;
+	
+	@Before
+	public void initialize() {
+		wos = new WikipediaOSuerte();
+	}
+	
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void busquedaEnWikipedia() throws IOException {
+		Assert.assertEquals("https://es.wikipedia.org/wiki/Bitcoin", wos.solve("Bitcoin"));
+		Assert.assertEquals("https://es.wikipedia.org/wiki/Ethereum", wos.solve("Ethereum"));
+		Assert.assertEquals("https://es.wikipedia.org/wiki/Steemit", wos.solve("Steemit"));
+	}
+	
+	@Test
+	public void busquedaConSuerte() throws IOException {
+		Assert.assertEquals("https://google.com.ar/search?btnI&q=Everipedia", wos.solve("Everipedia"));
+		Assert.assertEquals("https://google.com.ar/search?btnI&q=Lympo", wos.solve("Lympo"));
 	}
 
 }
